29be86aec1e151fc6226aedf3575beaffb997a27
clouddawn <1993456181@qq.com>
2021-08-22 08:33:01 +0800

    deploy

---
+++ precache-manifest.36a1273b939b141f69f6769a5b7fc924.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4875d1295726449423a",
+    "url": "/orange-website/css/app.92fbc816.css"
+  },
+  {
+    "revision": "d51b6d3a7e761f8f66659e7db0f34b2c",
+    "url": "/orange-website/img/loading.webp"
+  },
+  {
+    "revision": "5dbb8b89a6efee44f656b03ddc18cb76",
+    "url": "/orange-website/img/orange.png"
+  },
+  {
+    "revision": "fe02b2dfd5aae7def867e14a5ef25bf1",
+    "url": "/orange-website/index.html"
+  },
+  {
+    "revision": "e4875d1295726449423a",
+    "url": "/orange-website/js/app.0ade1849.js"
+  },
+  {
+    "revision": "f938fc2a7c398d4d9546",
+    "url": "/orange-website/js/chunk-vendors.fe5681b6.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/orange-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/orange-website/robots.txt"
+  }
+]);
